614556bf2e3c5ee585a136c9bd28982787ff78fe
Salinepng <salinengatha@gmail.com>
2024-11-06 14:10:52 +0300

    Add files via upload

---
+++ CAT2--QUIZ 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+int i, j;
+
+int scores [2] [2] = {{65,92,84,72} , {35,70,59,67}};
+
+for(i=0;i<2;i++){
+
+	for(j=0;j<2;j++){
+
+printf("scores %d %d", i,j, scores [i][j] );
+
+				}
+
+					}
+
+return 0;
+
+}
