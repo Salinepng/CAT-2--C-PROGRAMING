60124d91dd02f10bf2ecaa21891b306f07800b3b
Salinepng <salinengatha@gmail.com>
2024-11-08 09:22:14 +0300

    Update CAT2--QUIZ 2.c

--- CAT2--QUIZ 2.c
+++ CAT2--QUIZ 2.c
@@ -1,16 +1,17 @@
+
 #include<stdio.h>
 
 int main(){
 
 int i, j;
 
-int scores [2] [2] = {{65,92,84,72} , {35,70,59,67}};
+int scores[4] [2] = {{65,92},{84,72} ,{35,70},{59,67}};
 
-for(i=0;i<2;i++){
+for(i=0;i<4;i++){
 
 	for(j=0;j<2;j++){
 
-printf("scores %d %d", i,j, scores [i][j] );
+printf("scores [%d] [%d] =%d\n", i,j, scores[ i][j] );
 
 				}
 
